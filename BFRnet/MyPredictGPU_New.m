604221868be85fba564fc1ce2661a4e5ad27b714
Xuanyu Zhu <58714809+xuanyu4513947@users.noreply.github.com>
2022-04-07 00:40:22 +0800

    Add files via upload

---
+++ BFRnet/MyPredictGPU_New.m
@@ -0,0 +1,24 @@
+function [Q_pre] = MyPredictGPU_New(V)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+imSize= size(V); 
+
+load BFRnet_L2_64PS_24BS_45Epo_NewHCmix.mat
+newInput = image3dInputLayer(imSize,...
+    'Name','ImageInputLayer', 'Normalization', 'none');
+
+L1Net = net; 
+
+L1Net = replaceLayer(layerGraph(L1Net),'ImageInputLayer',newInput);
+L1Net = assembleNetwork(L1Net);
+
+clear net; 
+%% prediction
+
+predict(L1Net,  zeros(imSize), 'ExecutionEnvironment', 'auto'); % to pre-load the parameters into the memory;
+tic
+Q_pre = predict(L1Net,  V, 'ExecutionEnvironment', 'auto'); % Or switch 'auto' to 'gpu'/'multi-gpu' 
+toc
+
+end
+
