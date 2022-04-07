28bb1543cdfccc4c8379bac23153d17300c5661d
Xuanyu Zhu <58714809+xuanyu4513947@users.noreply.github.com>
2022-04-07 17:36:05 +0800

    Delete BFRnet/Block3DBN directory

--- BFRnet/Block3DBN/matRead.m
+++
@@ -1,8 +0,0 @@
-function data = matRead(filename)
-% data = matRead(filename) reads the image data in the MAT-file filename
-% data = matRead(filename) reads the image data in the NII-file filename
-
-nii = load_untouch_nii(filename);
-data = nii.img;
-
-end
--- BFRnet/Block3DBN/matRead_Original.m
+++
@@ -1,7 +0,0 @@
-function data = matRead(filename)
-% data = matRead(filename) reads the image data in the MAT-file filename
-
-inp = load(filename);
-f = fields(inp);
-data = inp.(f{1});
-end
