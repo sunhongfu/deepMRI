66d2e38c40f004bae983e2d198f2a8f939dbc735
Xuanyu Zhu <58714809+xuanyu4513947@users.noreply.github.com>
2022-04-12 13:52:36 +0800

    Update BFRnet_demo.m

--- BFRnet/Eval/BFRnet_demo.m
+++ BFRnet/Eval/BFRnet_demo.m
@@ -7,7 +7,7 @@ deepMRI_root = '~/Downloads/deepMRI-master'; % where deepMRI git repo is cloned 
 addpath(genpath([deepMRI_root,'/utils']));  %  add NIFTI saving and loading functions;
 
 
-NetPath = 'BFRnet_L2_64PS_24BS_45Epo_NewHCmix.mat.mat';  %% give your network path here. 
+NetPath = 'BFRnet_L2_64PS_24BS_45Epo_NewHCmix.mat';  %% give your network path here. 
 
 nii = load_nii('../../tfs.nii'); % load the total field map here, and replace the file name with yours.
 tfs = double(nii.img);
