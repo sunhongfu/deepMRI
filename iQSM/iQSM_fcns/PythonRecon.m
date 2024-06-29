c75a8bcd80074f89a25e7a5b783dc61cad6ee5b9
uqygao10 <yang.gao@uq.edu.au>
2024-06-29 13:46:42 +0800

    Minor bug fixes.

--- iQSM/iQSM_fcns/PythonRecon.m
+++ iQSM/iQSM_fcns/PythonRecon.m
@@ -12,18 +12,23 @@ if ispc
     ConfigPython;  %% configure pytorh for matlab terminal
 end
 
-disp('Calling Python for iQSM reconstruction'); 
+% getAbsFolderPath = @(y) string(unique(arrayfun(@(x) x.folder, dir(y), 'UniformOutput', false)));
+
+% InputPath = getAbsFolderPath(InputPath); 
+
+disp('Calling Python for QSM reconstruction'); 
 
 %% cd ../PythonCodes/
-[codeFolder, codeName, codeExt] = fileparts(PyFile); 
-cd(codeFolder); 
-
-CodeFile = [codeName codeExt]; 
-exe_command = sprintf("python -u '%s' -I '%s' -O '%s' -C '%s'", CodeFile, InputPath, OutPath, CheckpointsPath);
+% [codeFolder, codeName, codeExt] = fileparts(PyFile); 
+% cd(codeFolder); 
+% 
+% CodeFile = [codeName codeExt]; 
+% exe_command = sprintf('python -u %s -I %s -O %s -C %s', CodeFile, InputPath, OutPath, CheckpointsPath);
+exe_command = sprintf('python -u %s -I %s -O %s -C %s', PyFile, InputPath, OutPath, CheckpointsPath);
 
 system(exe_command); % !python -u Inference.py
 cd(curDir)
 
-disp('iQSM Reconstruction Finished');
 end
+
 
